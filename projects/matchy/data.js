655ccfcb409d4f24887ae9733abf3d7f55cd60ba
Chdaigle228 <cody.h.daigle@gmail.com>
2023-08-21 21:20:12 +0000

    matchy pt 1

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -32,26 +32,63 @@ animal['name'] = 'Carl';
 animal.noises = [];
 console.log(animal);
 
-
+/**
+ * 
+ * ## Step 2 - Array Creation
+ 1. [x ] Create a variable named `noises` and assign it to an empty array.
+ 2. [x ] Using **bracket notation** give `noises` it's first element. A string representing a sound your animal might make.
+ 3. [ x] Using an array function add another noise to the end of `noises`.
+ 4. [ x] Go to the array documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array?redirectlocale=en-US
+ 5. [ x] Look through the functions until you find the one that will place an element at the begining of the array.
+ 6. [ x] Add an element to `noises` using this function.
+ 7. [ ] Using **bracket syntax** again, add another element to the end of `noises`. Make sure that the way you do this step would work no matter how many elements `noises` had. In other words, don't hard code the position of the new element.
+ 8. [ ] `console.log` the length of `noises`
+ 9. [ ] `console.log` the last element in `noises` again without hard coding the index.
+ 10. [ ] `console.log` the whole array.
+ 11. [ ] Does it look right?
+ */
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+var noises = [];
+noises[0] = 'meow';
+noises.push('growl');
+noises.unshift('hiss');
+noises[noises.length] = 'pur';
+console.log(noises.length);
+console.log(noises[noises.length - 1]);
+console.log(noises);
 
 
 
+/**
+ * ## Step 3 - Combining Step 1 and 2
+ 1. [ ] Using **bracket syntax**, assign the `noises` property on `animal` to our new `noises` array.
+ 2. [ ] Using any syntax add another noise to the `noises` property on `animal`.
+ 3. [ ] `console.log` `animal`.
+ 4. [ ] Does it look right?
+ */
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+animal['noises'] = noises;
+animal.noises.push('hmmm');
+console.log(animal);
+//sure? 
 
-
-
+/**
+ * 
+ * ## Step 4 - Review
+ 1. [ ] What are the different ways you can access properties on objects?
+ 2. [ ] What are the different ways of accessing elements on  arrays?
+ */
 /* *******************************************************************
  * Step 4 - Review
  *
  * 1. What are the different ways you can access properties on objects?
- *
+ *  bracket notation and dot notation
  * 2. What are the different ways of accessing elements on arrays?
- *
+ *dot notation
  * *******************************************************************
  */
 
@@ -62,18 +99,78 @@ console.log(animal);
  * a rest when you can! Grab a drink and have a think!
  * For like 10 minutes, then, BACK TO WORK! :)
  * *******************************************************************
+ * 
+ * 
+ * ## Step 6 - A Collection of Animals
+ 1. [ ] Create a variable named `animals` and assign it to an empty array.
+ 2. [ ] `push` our `animal` that we created to `animals`. You should now see your first animal appear on your `index.html` page!
+ 3. [ ] `console.log` `animals`. What does it look like?
+ 4. [ ] Create a variable called `duck` and assign it to the data:
+  - `{ species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh'] }`
+ 5. [ ] `push` `duck` to `animals`
+ 6. [ ] `console.log` `animals`. What does it look like?
+ 7. [ ] Create two more animal objects each with a species, a name, and at least two sounds sounds and add each one of them to `animals.
+ 8. [ ] `console.log` `animals`, and, `console.log` the length of `animals`. Is everything looking right?
  */
 
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+var animals = [];
+animals.push(animal);
+console.log(animals);
+var duck = {
+  species: 'duck',
+  name: 'Jerome',
+  noises: ['quack', 'honk', 'sneeze', 'woosh']
+}
+animals.push(duck);
+console.log(animals);
+var panda = {
+  species: 'panda',
+  name: 'Gerome',
+  noises: ['munch', 'crunch', 'sneeze', 'snore']
+}
+animals.push(panda);
+var dog = {
+  species: 'dog',
+  name: 'Chloe',
+  noises: ['bark', 'huff', 'sneeze', 'snore']
+}
+animals.push(dog)
+console.log(animals.length)
 
+/**
+ * Imagine that our website has a profile page for each animal. On this profile page we can see a list of each animal's friend on the website. Just like how people have a list of friends or followers on facebook or instagram. What would be a good data structure to hold this list of friends?
 
+ 1. [ ] Choose a data structure for this **list** of friends.
+ 2. [ ] Write a comment in your code that explains why you chose this data structure.
+ 3. [ ] Create a variable called `friends` and assign it to the data structure that you chose.
+ 4. [ ] Take a look at the documentation for `Math.random` here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ 5. [ ] Write a function called `getRandom` that takes our `animals` array and returns  a random `index` of the input array, using `Math.random`
+ 6. [ ] Using a random index from this function that you just created, get a random animal and add its `name` to `friends`.
+ 7. [ ] `console.log` `friends`.
+ 8. [ ] Using **bracket notation**, add the `friends` list as a **property** also named `friends` on one of the animals in the `animals` array
+ 9. [ ] `console.log` your work.
+ */
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+var friends = []; //easiest to create a list in my opinion
+function getRandom(animals){
+  //animals array
+  //returns random index of array using Math.random
+   var ind = Math.floor(Math.random() * animals.length);
+   //returns random index of animals array
+   return ind;
+}
+var random = animals[getRandom(animals)];
+animals.friends = random.name;
+console.log(friends)
 
+animal['friends'] = friends;
+console.log(animals);
 
 
 /**

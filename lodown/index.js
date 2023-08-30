d6b576f204454fe75feea34e09d5fdd0cc14ea2b
Cody Daigle <cody.h.daigle@gmail.com>
2023-08-30 15:28:58 +0000

    EJCH3

---
+++ lodown/index.js
@@ -0,0 +1,549 @@
+'use strict';
+
+// YOU KNOW WHAT TO DO //
+
+/**
+ * each: Designed to loop over a collection, Array or Object, and applies the 
+ * action Function to each value in the collection.
+ * 
+ * @param {Array or Object} collection: The collection over which to iterate.
+ * @param {Function} action: The Function to be applied to each value in the 
+ * collection
+ */
+function each(collection, action) {
+    if(Array.isArray(collection)) {
+        for(var i = 0; i < collection.length; i++) {
+            action(collection[i], i, collection);
+        }
+    } else {
+        for (var key in collection) {
+            action(collection[key], key, collection);
+        }
+    }
+}
+module.exports.each = each;
+
+/**
+ * identity: takes in an input value and returns unchanged
+ * @param {value} Function takes in any value.
+ * @returns {value}
+ */
+
+function identity(value){
+    return value;
+}
+module.exports.identity = identity;
+
+/**
+ * 
+ * @param { Array or Object } Collection: function takes in an array or an object 
+ * @param { Function } func 
+ * @returns 
+ */
+
+_.map = function(list, func){
+    //new array
+    let array = [];
+//loop through array
+if(Array.isArray(list)){
+    for(let i = 0; i < list.length; i++){
+        array.push(func(list[i], i, list));
+        }
+
+}else{
+    for(let key in list){
+        array.push(func(list[key], key, list));
+        }
+    }
+    return array;
+}
+
+
+
+
+
+_.typeOf = function(value) {
+   //eliminate the 'objects'
+   //check if value is an array
+   if(Array.isArray(value)){
+    //log as 'array' says undefined instead...
+        return 'array';
+        //check if value is null
+   }else if(value === null){
+    //log null instead of object
+        return 'null';
+        //check if instance of date
+   }else if(value instanceof Date){
+    //log string of date
+        return 'date';
+   }else if(typeof value === 'object' && typeof value !== null){
+        return 'object'
+            //check for the simple types
+   }else if(typeof value !== 'object'){
+        //log the non-object datatypes
+        return typeof value;
+   }
+}
+
+
+/** _.first
+* Arguments:
+*   1) An array
+*   2) A number
+* Objectives:
+*   1) If <array> is not an array, return []
+*   2) If <number> is not given or not a number, return just the first element in <array>.
+*   3) Otherwise, return the first <number> items of <array>
+* Edge Cases:
+*   1) What if <number> is negative?
+*   2) What if <number> is greater than <array>.length?
+* Examples:
+*   _.first("ponies", 1) -> []
+*   _.first(["a", "b", "c"], "ponies") -> "a"
+*   _.first(["a", "b", "c"], 1) -> "a"
+*   _.first(["a", "b", "c"], 2) -> ["a", "b"]
+*/
+
+_.first = function(array, number){
+    //check if not array
+    if(!Array.isArray(array)){
+        return [];
+    }else if(number === undefined){
+        return array[0];
+    }else if(number < 0){
+        return [];
+    }else if(number > array.length){
+        return array
+    } else{
+        return array.slice(0, number);
+    }
+}
+
+
+/** _.last
+* Arguments:
+*   1) An array
+*   2) A number
+* Objectives:
+*   1) If <array> is not an array, return []
+*   2) If <number> is not given or not a number, return just the last element in <array>.
+*   3) Otherwise, return the last <number> items of <array>
+* Edge Cases:
+*   1) What if <number> is negative?
+*   2) What if <number> is greater than <array>.length?
+* Examples:
+*   _.last("ponies", 2) -> []
+*   _.last(["a", "b", "c"], "ponies") -> "c"
+*   _.last(["a", "b", "c"], 1) -> "c"
+*   _.last(["a", "b", "c"], 2) -> ["b", "c"]
+*/
+
+_.last = function(array, number){
+//check if array
+    if(!Array.isArray(array)){
+        return [];
+    }else if(number === undefined){
+        return array[array.length - 1];
+    }else if(number < 0){
+        return [];
+    }else if(number > array.length){
+        return array
+    } else{
+        return array.slice(1);
+    }
+}
+
+
+
+/** _.indexOf
+* Arguments:
+*   1) An array
+*   2) A value
+* Objectives:
+*   1) Return the index of <array> that is the first occurrance of <value>
+*   2) Return -1 if <value> is not in <array>
+*   3) Do not use [].indexOf()!
+* Edge Cases:
+*   1) What if <array> has multiple occurances of val?
+*   2) What if <val> isn't in <array>?
+* Examples:
+*   _.indexOf(["a","b","c"], "c") -> 2
+*   _.indexOf(["a","b","c"], "d") -> -1
+*/
+
+_.indexOf = function(array, value){
+    //iterate through array?
+    for(let i = 0; i < array.length; i++){
+        if(!array.includes(value)){
+            return -1
+        }else if(array[i] === value){
+            return i
+        }
+    }
+}
+
+/** _.contains
+* Arguments:
+*   1) An array
+*   2) A value
+* Objectives:
+*   1) Return true if <array> contains <value>
+*   2) Return false otherwise
+*   3) You must use the ternary operator in your implementation.
+* Edge Cases:
+*   1) did you use === ?
+*   2) what if no <value> is given?
+* Examples:
+*   _.contains([1,"two", 3.14], "two") -> true
+*/
+
+
+_.contains = function(array, value){
+   return array.includes(value) ? true : false
+}
+
+/** _.unique
+* Arguments:
+*   1) An array
+* Objectives:
+*   1) Return a new array of all elements from <array> with duplicates removed
+*   2) Use _.indexOf() from above
+* Examples:
+*   _.unique([1,2,2,4,5,6,5,2]) -> [1,2,4,5,6]
+*/
+
+_.unique = function(array){
+    return [...new Set(array)]
+}
+
+/** _.filter
+* Arguments:
+*   1) An array
+*   2) A function
+* Objectives:
+*   1) call <function> for each element in <array> passing the arguments:
+*      the element, it's index, <array>
+*   2) return a new array of elements for which calling <function> returned true
+* Edge Cases:
+*   1) What if <function> returns something other than true or false?
+* Examples:
+*   _.filter([1,2,3,4,5], function(x){return x%2 === 0}) -> [2,4]
+* Extra Credit:
+*   use _.each in your implementation
+*/
+
+_.filter = function(array, func){
+    //create array to store
+    let filtered = [];
+    //loop array
+    for(let i = 0; i < array.length; i++){
+        // is statement
+    if(func(array[i], i, array)){ 
+        filtered.push(array[i]);
+        }
+    }
+    return filtered
+}
+/** _.reject
+* Arguments:
+*   1) An array
+*   2) A function
+* Objectives:
+*   1) call <function> for each element in <array> passing the arguments:
+*      the element, it's index, <array>
+*   2) return a new array of elements for which calling <function> returned false
+*   3) This is the logical inverse if _.filter()
+* Examples:
+*   _.reject([1,2,3,4,5], function(e){return e%2 === 0}) -> [1,3,5]
+*/
+
+_.reject = function(array, func){
+    let rejected = [];
+    //loop array
+    for(let i = 0; i < array.length; i++){
+        // is statement
+    if(func(array[i], i, array) === false){ 
+        rejected.push(array[i]);
+        }
+    }
+    return rejected
+}
+
+/** _.partition
+* Arguments:
+*   1) An array
+*   2) A function
+* Objectives:
+*   1) Call <function> for each element in <array> passing it the arguments:
+*       element, key, <array>
+*   2) Return an array that is made up of 2 sub arrays:
+*       0) An array that contains all the values for which <function> returned something truthy
+*       1) An array that contains all the values for which <function> returned something falsy
+* Edge Cases:
+*   1) This is going to return an array of arrays.
+* Examples:
+*   _.partition([1,2,3,4,5], function(element,index,arr){
+*     return element % 2 === 0;
+*   }); -> [[2,4],[1,3,5]]
+}
+*/
+
+_.partition = function(array, func){
+    let trues = [];
+    let falses = [];
+    //loop array
+    for(let i = 0; i < array.length; i++){
+        if(func(array[i], i, array)){
+            trues.push(array[i]);
+        }else {
+            falses.push(array[i])
+        }
+    }
+
+    return [[...trues], [...falses]]
+}
+
+
+
+
+
+/** _.pluck
+* Arguments:
+*   1) An array of objects
+*   2) A property
+* Objectives:
+*   1) Return an array containing the value of <property> for every element in <array>
+*   2) You must use _.map() in your implementation.
+* Examples:
+*   _.pluck([{a: "one"}, {a: "two"}], "a") -> ["one", "two"]
+*/
+
+_.pluck = function(array, prop){
+    //create empty array
+  //iterate through array first?
+   let items = array.map(obj => {
+    return obj[prop];
+   })
+   return items
+    
+    
+}
+/** _.every
+* Arguments:
+*   1) A collection
+*   2) A function
+* Objectives:
+*   1) Call <function> for every element of <collection> with the paramaters:
+*      if <collection> is an array:
+*          current element, it's index, <collection>
+*      if <collection> is an object:
+*          current value, current key, <collection>
+*   2) If the return value of calling <function> for every element is true, return true
+*   3) If even one of them returns false, return false
+*   4) If <function> is not provided, return true if every element is truthy, otherwise return false
+* Edge Cases:
+*   1) what if <function> doesn't return a boolean
+*   2) What if <function> is not given?
+* Examples:
+*   _.every([2,4,6], function(e){return e % 2 === 0}) -> true
+*   _.every([1,2,3], function(e){return e % 2 === 0}) -> false
+*/
+
+_.every = function(list, func){
+    //determine if array
+    if(Array.isArray(list)){
+        //check if func is even provided
+        if(func === undefined){ //edge case //so it can still iterate to find anything false
+            //loop through array, then object
+            for(let i = 0; i < list.length; i++){
+                //check if the element does not exists
+                if(!list[i]){
+                    return false;
+                }
+            }
+            //else it is provided
+        }else{
+            for(let i = 0; i < list.length; i++){
+                //check if func invoke is false anywhere
+                if(!func(list[i], i, list)){
+                    //if it finds ANYTHING false
+                    return false;
+                }
+            }
+        }
+        //else it's an object...
+    }else{
+        //check if provided
+        if(func === undefined){ //edge case //so it can still iterate to find anything false
+            //loop over object
+            for(let key in list){
+                ////check if func invoke is false anywhere
+                if(!list[key]){
+                    //key does not exist
+                    return false;
+                }
+            }
+            //else it is provided
+        }else{
+            //iterate yet again...
+            for(let key in list){
+                //check if key exists
+                if(!list[key]){
+                    //does not
+                    return false;
+                }
+            }
+        }
+    }
+    //otherwise
+    return true //christ
+}
+
+/** _.some
+* Arguments:
+*   1) A collection
+*   2) A function
+* Objectives:
+*   1) Call <function> for every element of <collection> with the paramaters:
+*       if <collection> is an array:
+*        current element, it's index, <collection>
+*       if <collection> is an object:
+*        current value, current key, <collection>
+*   2) If the return value of calling <function> is true for at least one element, return true
+*   3) If it is false for all elements, return false
+*   4) If <function> is not provided return true if at least one element is truthy, otherwise return false
+* Edge Cases:
+*   1) what if <function> doesn't return a boolean
+*   2) What if <function> is not given?
+* Examples:
+*   _.some([1,3,5], function(e){return e % 2 === 0}) -> false
+*   _.some([1,2,3], function(e){return e % 2 === 0}) -> true
+*/
+
+_.some = function(list, func){
+       //determine if array
+       if(Array.isArray(list)){
+        //check if func is even provided
+        if(func === undefined){ //edge case //so it can still iterate to find anything false
+            //loop through array, then object
+            for(let i = 0; i < list.length; i++){
+                //check if the element exists
+                if(list[i]){ //removed !
+                    return true; //changed to true
+                }
+            }
+            //else it is provided
+        }else{
+            for(let i = 0; i < list.length; i++){
+                //check if func invoke is true anywhere
+                if(func(list[i], i, list)){ //removed !
+                    //if it finds ANYTHING false
+                    return true; //changed to true
+                }
+            }
+        }
+        //else it's an object...
+    }else{
+        //check if provided
+        if(func === undefined){ //edge case //so it can still iterate to find anything false
+            //loop over object
+            for(let key in list){
+                ////check if func invoke is true anywhere
+                if(list[key]){ //removed !
+                    //key does not exist
+                    return true; //changed to true
+                }
+            }
+            //else it is provided
+        }else{
+            //iterate yet again...
+            for(let key in list){
+                //check if key exists
+                if(list[key]){  //removed !
+                    //does not
+                    return true; //change to true
+                }
+            }
+        }
+    }
+    //otherwise
+    return false; //changed to false
+}
+
+
+/** _.reduce
+* Arguments:
+*   1) An array
+*   2) A function
+*   3) A seed
+* Objectives:
+*   1) Call <function> for every element in <collection> passing the arguments:
+*         previous result, element, index
+*   2) Use the return value of <function> as the "previous result"
+*      for the next iteration
+*   3) On the very first iteration, use <seed> as the "previous result"
+*   4) If no <seed> was given, use the first element/value of <collection> as <seed> and continue to the next element
+*   5) After the last iteration, return the return value of the final <function> call
+* Edge Cases:
+*   1) What if <seed> is not given?
+* Examples:
+*   _.reduce([1,2,3], function(previousSum, currentValue, currentIndex){ return previousSum + currentValue }, 0) -> 6
+*/
+
+_.reduce = function(array, func, seed){
+    let result;
+    //check if seed is  not given
+    if(seed === undefined){
+        result = array[0];
+            //loop through array
+            for(let i = 1; i < array.length; i++){ //forgot the 1 for the initial item at index 0
+                //result is the callbackfn with it's previous value being the next iteration starting point
+                result = func(result, array[i], i, array);
+            }
+    //else it is given   
+    }else{
+        result = seed;
+            //loop through array with seed starting point
+            for(let i = 0; i < array.length; i++){
+                //result is the callback fn, (last element, element value, index, array)
+                result = func(result, array[i], i, array);
+            }
+        }
+        //return final value as result
+        return result
+    
+}
+
+
+/** _.extend
+* Arguments:
+*   1) An Object
+*   2) An Object
+*   ...Possibly more objects
+* Objectives:
+*   1) Copy properties from <object 2> to <object 1>
+*   2) If more objects are passed in, copy their properties to <object 1> as well, in the order they are passed in.
+*   3) Return the update <object 1>
+* Examples:
+*   var data = {a:"one"};
+*   _.extend(data, {b:"two"}); -> data now equals {a:"one",b:"two"}
+*   _.extend(data, {a:"two"}); -> data now equals {a:"two"}
+*/
+
+_.extend = function(object1, object2, more){
+    //use blank parameter becuse the assign method can assign anything to its combined object
+    let copyObject1 = Object.assign(object1, object2, more);
+    
+    return copyObject1
+}
+//nice
+
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+if((typeof process !== 'undefined') &&
+   (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports = _;
+}

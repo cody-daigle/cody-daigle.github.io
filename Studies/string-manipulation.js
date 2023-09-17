fbc80a114ec4a1246ee095f8e0b10328a37e4d8a
Cody Daigle <cody.h.daigle@gmail.com>
2023-09-17 19:06:14 +0000

    gitPod done

--- Studies/string-manipulation.js
+++ Studies/string-manipulation.js
@@ -4,10 +4,60 @@
  *  
  * 0. Strings are primative datatypes that store data in text form and are used to to represent and manipulate characters.
  *  A string is text within either '' single quotes or "" double quotes. They can also created with ``backticks as 
- *  template literals.
- * with operators
- * with string methods
+ *  template literals. It is important to to undestand that strings are immutable, meaning the content's cannot be changed
+ *  once created. However, in JavaScript, there are many ways to manipulte this data and return new pieces of data.
  * 
- * 
- * 
- */
+ * 1. Operators 
+ *      In JavaScript there are two string operators that are used to concatenate strings together.
+ *      The concatenate operator '+' with add strings together and create a whole new string that will add the contents of the strings
+ *      the operator was used with. This is called string concatenation, which will start the the first letter of the additional string
+ *      immediately after the last letter in the first string.
+*/  let firstName = 'Cody';
+    let lastName = 'Daigle';
+    let fullName = firstName + lastName; //No space to show how immediate the second string follows the first.
+    console.log(fullName) //logs CodyDaigle 
+
+/**
+ * 2. String Methods:
+ *      There may only be a few string operators, but strings are not lacking in the 'method' department. 
+ *      Ironically there is also a concat method that is used just like the operators above.
+ *      JavaScript has various methods of manipulation that are frequently used:
+ */   
+ //     String.concat() - Combines two or more strings together into a new string.
+           console.log(firstName.concat(lastName)) //logs CodyDaigle 
+
+ //     String.toUpperCase() - Changes the string to have all uppercase letters.
+           console.log(firstName.toUpperCase()); //logs CODY
+
+ //     String.toLowerCase() - Changes the string to have all lowercsae letters.
+            console.log(firstName.toLowerCase()); //logs cody
+
+ //     String.slice() - Removes a portion of a string and returns a new string.
+            console.log(firstName.slice(1)); //logs ody
+
+ //     String.split() - Turns the string into an array of substrings
+            console.log(first.split('')); //logs ['c', 'o', 'd', 'y', 'D', 'a', 'i', 'g', 'l', 'e']
+
+ //     String.length - Returns the length of the string, also including spaces.
+            console.log(firstName.length); //logs a length of 4
+
+ //     String.charAt() - Returns the character at the provided index.
+            console.log(firstName.charAt(2)); //logs 'd'
+
+ //     String.indexOf() - Returns the index of the first occurrence of a substring that is within a string. Otherwise returns -1.
+            console.log(fullName.indexOf('d')); //logs 2
+
+ //     String.lastIndexOf() - Returns the index of the last occurrence of a substring within a string. Otherwise returns -1.
+            console.log(firstName.lastIndexOf('ai')); //logs -1 becuase 'ai' does not exist in firstName
+
+ //     String.replace() - Used to search a string to replace a substring with a replacement or can be used with regex for more a narrowed search.
+            console.log(fullName.replace('y', 'y ')); //replace the y character with a y and a space logs Cody Daigle
+
+ //     String.search() - Searches for a match of a regex and returns the index of the match or it returns -1.
+            console.log(firstName.search(/[a-z]/g)); //seach for lowercase letter. Logs index 1.
+
+ //     String.match() - Accepts regex and is matched against a string.
+            console.log(fullName.match(/[^a-z]/g)); // Logs ['C', 'D'] when using match on non-lowerCase letters
+
+ //     String.subString() - A returned portion of a string from a provided start and ending index.
+            console.log(fullName.substring(0, 5)); //CodyD
---
+++ stringMethods.js
@@ -0,0 +1,39 @@
+/**
+ * * String.at() - Method that takes in a positive or negative integer and returns a new string consisting of a single UTF-16 code at that specified offset.
+ * String.charAt() - Method that takes in an integer and returns a new string consisiting of a single UTF-16 code at that given index.
+ * String.charCodeAt() - Returns an integer between 0-65535 representingthe UTF-16 code at that given index.
+ * String.codePointAt() - Returns a positive integer that represents the Unicode code point value of the character that that index given index.
+ * String.concat() - Concatenates the string arguments and returns a new string.
+ * String.endsWith() - Checks whether a string ends with the specified characters, returning a boolean.
+ * String.fromCharCode() - A static method that returns a string created by the provided UTF-16 code units.
+ * String.fromCodePoint() - This static method that returns a string created by the provided sequence of code units.
+ * String.includes() - Performs a case-sensative search to check if a given string can be found within a string and returns a boolean.
+ * String.indexOf() - performs a search through a string and at the first occurrence of the specified substring it will return the index.
+ * String.isWellFormed() - Checks whether a string has any lone surrogates and will return a boolean based on the search.
+ * String.lastIndexOf() - Similar to indexOf, but will return the index of the last occurence of a provided substring.
+ * String.localeCompare() - Determines if a string comes before, after, or is the same as the provided string, in sort order.
+ * String.match() - values retrieves the result of matching this string against
+ * String.matchAll() - values returns an iterator of all results matching this string against a regular expression, including capturing groups.
+ * String.normalize() - values returns the Unicode Normalization Form of this string.
+ * String.padEnd() - values pads this string with a given string (repeated, if needed) so that the resulting string reaches a given length. The padding is applied from the end of this string.
+ * String.padStart() - values pads this string with another string (multiple times, if needed) until the resulting string reaches the given length. The padding is applied from the start of this string.
+ * String.raw() - This is a tag function for template literals.
+ * String.repeat() - values constructs and returns a new string which contains the specified number of copies of this string, concatenated together.
+ * String.replace() -values returns a new string with one, some, or all matches of a pattern replaced by a replacement. String or regex.
+ * String.replaceAll() - values returns a new string with all matches of a pattern replaced by a replacement.
+ * String.search() - values executes a search for a match between a regular expression and this string, returning the index of the first match in the string.
+ * String.slice() - values extracts a section of this string and returns it as a new string, without modifying the original string.
+ * String.split() - values takes a pattern and divides this string into an ordered list of substrings by searching for the pattern, puts these substrings into an array, and returns the array.
+ * String.startsWith() - Checks whether a string starts with the specified characters, returning a boolean.
+ * String.substring() - values returns the part of this string from the start index up to and excluding the end index
+ * String.toLocaleLowerCase() - values returns this string converted to lower case, according to any locale-specific case mappings.
+ * String.toLocaleUpperCase() - values returns this string converted to upper case, according to any locale-specific case mappings.
+ * String.toLowerCase()- values returns this string converted to lower case.
+ * String.toString() - values returns this string value.
+ * String.toUpperCase() -  values returns this string converted to uppercase.
+ * String.toWellFormed() - values returns a string where all lone surrogates of this string are replaced with the Unicode replacement character U+FFFD.
+ * String.trim() - values removes whitespace from both ends of this string and returns a new string, without modifying the original string.
+ * String.trimEnd() - values removes whitespace from the end of this string and returns a new string, without modifying the original string
+ * String.trimStart() - values removes whitespace from the beginning of this string and returns a new string, without modifying the original string
+ * String.valueOf() - values returns this string value.
+ */
